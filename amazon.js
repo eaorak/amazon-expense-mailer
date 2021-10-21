d7bdc6deeb0cca4f3925d74f5473171b44d39a9a
Ender Aydin Orak <ender.orak@johnlewis.co.uk>
2021-10-21 10:19:07 +0100

    fix

--- amazon.js
+++ amazon.js
@@ -8,7 +8,7 @@ const sendInvoice = require('./email')
 
 const AMAZON_STORE = 'https://www.amazon.co.uk'
 const INVOICE_PATH = './invoice.pdf'
-const PUPPETEER_CONFIG = {headless: true}
+const PUPPETEER_CONFIG = { headless: true }
 
 try {
   (async () => {
@@ -19,8 +19,8 @@ try {
     await page.goto(AMAZON_STORE)
     try {
       await page.click('#sp-cc-accept')
-    } catch (err) {}
-    
+    } catch (err) { }
+
     console.log(`Logging in to Amazon with '${process.env.AMAZON_EMAIL}'...`)
     // Login
     await page.click('#nav-link-accountList')
@@ -32,7 +32,7 @@ try {
     await page.waitForSelector('#ap_password')
     await page.type('#ap_password', process.env.AMAZON_PASSWORD)
     await page.click('#signInSubmit')
-    
+
     console.log('Navigating to account...')
     // Account
     await page.waitForSelector('#nav-link-accountList')
@@ -42,25 +42,25 @@ try {
     console.log('Navigating to orders...')
     await page.waitForSelector('a[href*="order-history"]')
     await page.click('a[href*="order-history"]')
-    
+
     // Click Invoice
     await page.waitForTimeout(5000);
     await page.waitForXPath("//*[contains(text(),'Invoice')]")
     const invoices = await page.$x("//*[contains(text(),'Invoice')]")
-    await invoices[0].click() 
-    
+    await invoices[0].click()
+
     // Click Order Summary
     console.log('Waiting for Invoices...')
     await page.waitForTimeout(5000);
     await page.waitForSelector('#a-popover-content-1 > ul > li:nth-child(1) > span > a')
     await page.click('#a-popover-content-1 > ul > li:nth-child(1) > span > a')
-    
+
     console.log(`Saving invoice to ${INVOICE_PATH}...`)
-    await page.waitForXPath("//*[contains(text(),'Final Details for Order')]")
+    await page.waitForXPath("//*[contains(text(),'Amazon.co.uk order number')]")
     await page.pdf({ path: INVOICE_PATH, format: 'A4' });
-    
+
     await browser.close()
-    
+
     // Sending invoice
     console.log('Sending invoice..')
     await sendInvoice([INVOICE_PATH])
